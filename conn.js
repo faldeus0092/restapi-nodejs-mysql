9e4493969af86c5d5d44f295b9ae47cf0c40fa4f
faldeus0092 <gavinbagus123@gmail.com>
2021-05-25 12:54:54 +0700

    config nodejs to connect clearDB

--- conn.js
+++ conn.js
@@ -1,11 +1,12 @@
 var mysql = require('mysql');
 
 // koneksi ke database
+// mysql://b172dba8ad5895:70c3c3ae@us-cdbr-east-03.cleardb.com/heroku_a64de6f5af87faa?reconnect=true
 const conn = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'restapidb'
+    host:'us-cdbr-east-03.cleardb.com',
+    user:'b172dba8ad5895',
+    password:'70c3c3ae',
+    database:'heroku_a64de6f5af87faa'
 });
 
 conn.connect((err) =>{
