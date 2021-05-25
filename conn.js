c19f04a38ef1e7bfac2a37de0a0876f06598e9c7
faldeus0092 <gavinbagus123@gmail.com>
2021-05-26 03:15:06 +0700

    disable ssl rejectUnauthorized

--- conn.js
+++ conn.js
@@ -2,12 +2,15 @@ var mysql = require('mysql');
 
 // koneksi ke database
 // mysql://b172dba8ad5895:70c3c3ae@us-cdbr-east-03.cleardb.com/heroku_a64de6f5af87faa?reconnect=true
-
+//mysql --host=us-cdbr-east-03.cleardb.com --user=b172dba8ad5895 --password=70c3c3ae --reconnect heroku_a64de6f5af87faa
 const conn = mysql.createPool({
     host:'us-cdbr-east-03.cleardb.com',
     user:'b172dba8ad5895',
     password:'70c3c3ae',
-    database:'heroku_a64de6f5af87faa'
+    database:'heroku_a64de6f5af87faa',
+    ssl : {
+        rejectUnauthorized: false
+     }
 });
 
 // const conn = mysql.createPool({
