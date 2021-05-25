05a17d2be6a61f78a64a1df2b2cd91754b76e067
faldeus0092 <gavinbagus123@gmail.com>
2021-05-25 16:58:29 +0700

    connection pooling

--- conn.js
+++ conn.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 
 // koneksi ke database
 // mysql://b172dba8ad5895:70c3c3ae@us-cdbr-east-03.cleardb.com/heroku_a64de6f5af87faa?reconnect=true
-const conn = mysql.createConnection({
+const conn = mysql.createPool({
     host:'us-cdbr-east-03.cleardb.com',
     user:'b172dba8ad5895',
     password:'70c3c3ae',
