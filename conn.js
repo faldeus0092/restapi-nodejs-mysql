e75b21caa4928cab6ed4838135b141abe3a13e20
faldeus0092 <73151823+faldeus0092@users.noreply.github.com>
2021-05-27 18:36:21 +0700

    removing credentials

--- conn.js
+++ conn.js
@@ -1,9 +1,7 @@
 const mysql = require('mysql');
 const dbConfig = require('./config/db.config');
 
-// koneksi ke database
-// mysql://b172dba8ad5895:70c3c3ae@us-cdbr-east-03.cleardb.com/heroku_a64de6f5af87faa?reconnect=true
-// mysql --host=us-cdbr-east-03.cleardb.com --user=b172dba8ad5895 --password=70c3c3ae --reconnect heroku_a64de6f5af87faa
+// connect to database
 
 var conn = mysql.createPool({
     host: dbConfig.HOST,
