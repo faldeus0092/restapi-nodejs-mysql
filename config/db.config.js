2128fba203123bf486a25496fdf1752bffb7b0a4
faldeus0092 <73151823+faldeus0092@users.noreply.github.com>
2021-05-27 18:38:20 +0700

    removing the credentials

--- config/db.config.js
+++ config/db.config.js
@@ -1,9 +1,12 @@
+// change the credentials based on your own
 module.exports = {
     HOST: "us-cdbr-east-03.cleardb.com",
-    USER: "b172dba8ad5895",
-    PASSWORD: "70c3c3ae",
-    DB: "heroku_a64de6f5af87faa"
+    USER: "b172dbxxxxxxxx",
+    PASSWORD: "70cxxxxx",
+    DB: "heroku_a64de6fxxxxxxxx"
 };
+
+// for running it locally
 // module.exports = {
 //     HOST: "localhost",
 //     USER: "root",
